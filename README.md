bee4a5f21651508302b7480d29af738595b41f58
Anthony Swierkosz <aswierkosz1@gmail.com>
2022-01-31 10:00:57 -0500

    Intial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# swierkosz.dev
+
+[swierkosz.dev](https://swierkosz.dev) is my personal website and this is its source code.
+
+The website is extremely bare-bones, but I want to get something more prominent up hopefully within
+the next few months. For now, I have focused on truly learning and understanding webpack and
+building a better configuration with up-to-date Webpack 5 features. I also want to use typescript
+for this website.
+
+FYI [web.dev](https://web.dev/) is a great source of knowledge.
+
---
+++ webpack.common.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const entries = {
+  index: './src/index.ts'
+}
+
+module.exports = {
+  entry: entries,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Swierkosz',
+      template: './src/home/index.html',
+      'meta': {
+        'viewport': 'width=device-width, initial-scale=1',
+        'description': 'Simple timestamp calculator website',
+      }
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          // fallback to style-loader in development
+          process.env.NODE_ENV !== "production"
+              ? "style-loader"
+              : MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+}
